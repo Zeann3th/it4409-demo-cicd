77a7ef343dacebd45951777d8a29898661e6d2c7
zeann3th <longhienly112@gmail.com>
2025-04-21 16:41:28 +0700

    feat: adding test comment

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 
+// Adding comments
 const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 const cors = require("cors");
 app.use(cors());
